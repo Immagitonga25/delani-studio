1bc49e589528e779a344a768b29698309b3dc3c6
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-19 00:24:20 +0300

    add a jquery hide/show method to the 'what we do' section to toggle between the icon and description when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
 //Business (back-end) logic
+
+
+
+
+
+
+
+
+
+
+
+//User (front-end) logic
+$(document).ready(function(){
+    $(".product").click(function() {
+        $(".product-hidden").toggle();
+        $(".product-showing").toggle();
+    });
+    $(".development").click(function() {
+        $(".development-hidden").toggle();
+        $(".development-showing").toggle();
+    });
+    $(".design").click(function() {
+        $(".design-hidden").toggle();
+        $(".design-showing").toggle();
+    });
+});
